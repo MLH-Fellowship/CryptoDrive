ebd9e5e8ad651ddb4c6530e2b9bccf2fe8a15d8a
imabp <abir.pal899@gmail.com>
2021-03-24 23:43:28 +0530

    Add Client Side Boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <h1>CryptoDrive</h1>
+      </center>
+
+    </div>
+  );
+}
+
+export default App;
