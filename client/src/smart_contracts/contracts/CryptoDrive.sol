7a6bad72f9a715ff2442581ff069c94d8e0d030e
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-03-25 14:40:05 +0530

    Added the Passhash and Public Key to store in the smart contract

--- client/src/smart_contracts/contracts/CryptoDrive.sol
+++ client/src/smart_contracts/contracts/CryptoDrive.sol
@@ -1,6 +1,4 @@
-// SPDX-License-Identifier: MIT
 pragma solidity >0.5.11;
-pragma experimental ABIEncoderV2;
 
 contract CryptoDrive{
     // Structure for storing the filehash,filename as File 
@@ -14,6 +12,8 @@ contract CryptoDrive{
     // Mapping is private
     
     mapping(string=>File[]) private FileHashes;
+    mapping(string=>string) private PassHash;
+    mapping(string=>string) private PublicKeys;
     
     // Function AddFileHash which takes user id , filehash , filename
     // It is pushes the structure in Filehash Mapping
@@ -28,6 +28,26 @@ contract CryptoDrive{
     function GetFilehash(string memory user_id) public view returns (File[] memory){
         return FileHashes[user_id];
     }
+    
+    // Funtion to Save the user id , pass hash, public keys in the different mappings while SignUp
+    
+    function SignUp(string memory user_id,string memory pass_hash,string memory public_keys) public{
+        PassHash[user_id]=pass_hash;
+        PublicKeys[user_id]=public_keys;
+    }
+    
+    // Function to GetPassHash which takes user id as input and returns PassHash which is saved earlier
+    
+    function GetPassHash(string memory user_id) public view returns (string memory){
+        return PassHash[user_id];
+    }
+    
+    // Function to get public keys for a particular input user id
+    
+    function getPublicKey(string memory user_id) public view returns (string memory){
+        return PublicKeys[user_id];
+    }
+    
     
     
 }
