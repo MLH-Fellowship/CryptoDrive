419ee25e1ac219b9f778914ebbfdc30c06cd69a9
imabp <abir.pal899@gmail.com>
2021-03-25 23:26:14 +0530

    Add Basic Functionality. Uploading to IPFS Locally

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+// Import React FilePond
+import { FilePond, registerPlugin } from "react-filepond";
 
+// Import FilePond styles
+import "filepond/dist/filepond.min.css";
 
-function App() {
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
+
+export default function App() {
+  const [files, setFiles] = useState([]);
   return (
-    <div className="App">
-      <center>
-        <h1>CryptoDrive</h1>
-      </center>
-
+    <div className="App" style={{width:"300px"}}>
+      <FilePond
+        files={files}
+        allowReorder={true}
+        allowMultiple={true}
+        server="http://localhost:8000/upload"
+        onupdatefiles={setFiles}
+        labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+        name="file"
+      />
     </div>
   );
 }
-
-export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
+    <h1>CryptoDrive</h1>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
