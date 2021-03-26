271dc18a3e24202a12982d1c4f9ee8185ecd01c1
imabp <abir.pal899@gmail.com>
2021-03-26 19:13:14 +0530

    Add Boilerplate for Auth

---
+++ client/src/components/AuthUIs/Login/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {TextField} from '@material-ui/core'
+const Login=()=>{
+
+    const [username,setUsername] = React.useState("") 
+    return(
+        <>
+        <TextField label="Enter A Username" value={username} onChange={(e)=>{setUsername(e.target.value)}}/>
+        </>
+    )
+}
+export default Login
