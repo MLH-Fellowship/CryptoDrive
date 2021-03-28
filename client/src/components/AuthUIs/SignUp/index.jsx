8d648417399d53d52033870938638e906569fa4f
imabp <abir.pal899@gmail.com>
2021-03-28 23:45:49 +0530

    Add SignUp Integration with GenerateKeyPair

--- client/src/components/AuthUIs/SignUp/index.jsx
+++ client/src/components/AuthUIs/SignUp/index.jsx
@@ -6,14 +6,16 @@ import EncrptPublicKey from "./../../Cryptography/Encryption";
 import DecryptPrivateKey from "./../../Cryptography/Decryption";
 import StringUpload from "./../../Ipfs/StringUpload";
 import StringRetrive from "./../../Ipfs/StringRetrive";
+import CircularProgress from "@material-ui/core/CircularProgress";
 const NodeRSA = require("node-rsa");
+
 const SignUp = () => {
   const [username, setUsername] = React.useState("");
   const [pubKey, setPubKey] = React.useState("");
   const [privateKey, setPrivate] = React.useState("");
   const [contract, setContract] = React.useState("");
   const [error, setError] = React.useState("");
-
+  const [loader, setLoader] = React.useState(false);
   React.useEffect(async () => {
     await loadWeb3();
     console.log("Web3 Loaded");
@@ -24,26 +26,35 @@ const SignUp = () => {
 
   const generateKeyPair = async () => {
     if (username) {
+      setLoader(true);
       setError(false);
-      const key = await new NodeRSA({ b: 2048 });
+      setPrivate(false);
+      setPubKey(false);
+      const key = new NodeRSA({ b: 512 });
       const public_key = key.exportKey("public");
       const private_key = key.exportKey("private");
       setPrivate(private_key);
       setPubKey(public_key);
-      const encrypted_text = await EncrptPublicKey("username", public_key);
+      const encrypted_text = await EncrptPublicKey(username, public_key);
+
       const hash = await StringUpload(encrypted_text);
-      console.log(hash);
       const encrypted_retrived_string = await StringRetrive(hash);
       const decrypted_text = await DecryptPrivateKey(
         encrypted_retrived_string,
         private_key
       );
-      console.log(decrypted_text);
-      console.log(decrypted_text === "username");
+      console.log(decrypted_text === username);
+   
     } else {
+      setLoader(false);
       setError("Enter a valid username");
     }
   };
+
+  // const EncryptData=()=>{
+  //   if()
+  //   const userEncryption = await EncrptPublicKey(username, pubKey)
+  // }
 
   return (
     <Grid container spacing={4}>
@@ -76,6 +87,7 @@ const SignUp = () => {
         </Button>
       </Grid>
       <Grid item xs={12} sm={12} md={12} lg={12}>
+        {!pubKey && !privateKey && loader && <CircularProgress />}
         {pubKey && (
           <h3>
             Public Key
@@ -110,7 +122,13 @@ const SignUp = () => {
         )}
         <br />
         <br />
-        {error && <span style={{ color: "red" }}><center><h2>{error}</h2></center></span>}
+        {error && (
+          <span style={{ color: "red" }}>
+            <center>
+              <h2>{error}</h2>
+            </center>
+          </span>
+        )}
       </Grid>
     </Grid>
   );
