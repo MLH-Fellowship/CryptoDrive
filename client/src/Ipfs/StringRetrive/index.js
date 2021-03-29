452e3722cc7b3b4055c42d39e7dcf51b0605307a
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2021-03-29 09:57:26 +0530

    fixed broken userhash

--- client/src/Ipfs/StringRetrive/index.js
+++ client/src/Ipfs/StringRetrive/index.js
@@ -1,6 +1,6 @@
 import ipfs from "../ipfs";
-const StringRetrive = (hash) => {
-  const data = ipfs.get(hash);
+const StringRetrive = async (hash) => {
+  const data = await ipfs.get(hash);
   const content = data[0].content;
   const retrived_string = content.toString("utf8");
   console.log(retrived_string);
