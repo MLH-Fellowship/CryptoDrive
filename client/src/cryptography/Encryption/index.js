65df8a573c8b1f9866368dfc7bdce8ff060dd88f
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-04-01 11:58:23 +0530

    Encrption File using NodeRSA

--- client/src/cryptography/Encryption/index.js
+++ client/src/cryptography/Encryption/index.js
@@ -2,6 +2,7 @@ import { publicEncrypt } from "crypto";
 
 const EncrptPublicKey = (text, public_key) => {
   var buffer = Buffer.from(text);
+  console.log(buffer);
   const encrypted = publicEncrypt(public_key, buffer);
   const encrypted_encoded = encrypted.toString("base64");
   return encrypted_encoded;
