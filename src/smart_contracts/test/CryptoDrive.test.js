b0f10144f7b428a195f7844af15e9ef9d1db4061
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-04-02 14:06:03 +0530

    Tests written to create new user u2

--- src/smart_contracts/test/CryptoDrive.test.js
+++ src/smart_contracts/test/CryptoDrive.test.js
@@ -21,16 +21,16 @@ contract('CryptoDrive', (accounts) => {
     const passhash="whdkwuxdhdrmi";
     const public_key="hhhhhhhhhhhssssssssseeee"
 
-    it('Signup Successfully',async()=>{
+    it('Signup Successfully for user 1',async()=>{
         const result=await cryptodrive.SignUp(user_id,passhash,public_key);
     });
 
-    it('Retrive Pass Hash',async()=>{
+    it('Retrive Pass Hash for user 1',async()=>{
         const result=await cryptodrive.GetPassHash(user_id);
         asserted.equal(passhash,result,"Pass Hash not Retrived Successfully");
     });
 
-    it('Retrive the Public Key',async()=>{
+    it('Retrive the Public Key for user 1',async()=>{
         const result=await cryptodrive.getPublicKey(user_id);
         asserted.equal(public_key,result,"Public Key not Retrived Successfully");
     });
@@ -38,7 +38,7 @@ contract('CryptoDrive', (accounts) => {
     const filehash="aaaaaaaawwwwwww0dddddd";
     const filename="name.txt"
 
-    it('Add the file hashes', async () => {
+    it('Add the file hashes by user 1', async () => {
         const result= await cryptodrive.AddFileHash(user_id,filehash,filename);
       });
 
@@ -48,6 +48,25 @@ contract('CryptoDrive', (accounts) => {
         asserted.equal(filename,result[0]['filename'],"File Name not Successfully Retrived");
     });
 
+    
+    const user_id_2="u2";
+    const passhash_2="aaawwwwaad";
+    const public_key_2="kkkkaaaaaeeeeeddddcccccssss"
+
+    
+    it('Signup Successfully for user 2',async()=>{
+      const result_2=await cryptodrive.SignUp(user_id_2,passhash_2,public_key_2);
+  });
+
+  it('Retrive Pass Hash for user 2',async()=>{
+      const result_2=await cryptodrive.GetPassHash(user_id_2);
+      asserted.equal(passhash_2,result_2,"Pass Hash not Retrived Successfully");
+  });
+
+  it('Retrive the Public Key for user 2',async()=>{
+      const result_2=await cryptodrive.getPublicKey(user_id_2);
+      asserted.equal(public_key_2,result_2,"Public Key not Retrived Successfully");
+  });
     
 
 
