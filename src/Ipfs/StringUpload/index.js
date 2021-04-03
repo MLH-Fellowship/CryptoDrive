90e12eac570667f78a271462babaf1c02b88ef41
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-04-03 21:42:18 +0530

    Buffer of decrypted file added

--- src/Ipfs/StringUpload/index.js
+++ src/Ipfs/StringUpload/index.js
@@ -1,7 +1,9 @@
 import ipfs from "../ipfs";
 
 const StringUpload = (data) => {
-  var buffer = Buffer.from(data);
+  var buffer = new Buffer(data);
+  console.log("Buffer:")
+  console.log(buffer)
   const hash = ipfs.add(buffer).then(
     (ipfsHash) => {
       return ipfsHash[0].hash;
