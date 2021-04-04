3e38343137f2cbfa55fe291081825e7e39265acc
imabp <abir.pal899@gmail.com>
2021-04-04 23:57:10 +0530

    Updated Background

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -74,7 +74,6 @@ function a11yProps(index) {
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    backgroundColor: theme.palette.background.paper,
     display: "flex",
   },
   tabs: {
