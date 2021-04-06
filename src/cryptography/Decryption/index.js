340a843251424699e25523ebb1546e5bfb93fd88
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-04-06 17:47:17 +0530

    refractored cryptography functions into one module

--- src/cryptography/Decryption/index.js
+++
@@ -1,9 +0,0 @@
-const NodeRSA = require('node-rsa');
-
-const DefaultDecryptPrivateKey = async(text, private_key) => {
-  private_key = await new NodeRSA(private_key);
-  const decrypted=await private_key.decrypt(text, 'utf8')
-  return decrypted;
-}
-
-export default DefaultDecryptPrivateKey;
