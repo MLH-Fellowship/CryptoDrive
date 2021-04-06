09a8141a602f1239274b39c109c6edf4bba5620d
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-04-06 23:08:49 +0530

    comments for web3 files added

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
-
 import "./pages/Landing/src/assets/scss/style.scss";
 
 ReactDOM.render(
