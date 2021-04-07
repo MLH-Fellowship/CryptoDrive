72b1c977d137af76f4c26419059aff28cb08a23e
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-04-07 17:44:48 +0530

    basename removed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default function App() {
 
   return (
     <>
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Switch>
         <Route exact path="/">
           <Landing />
