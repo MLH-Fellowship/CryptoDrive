c73a9e8a663ba84a26a86fa425dd7224269489fc
ksdkamesh99 <ksdkamesh99@gmail.com>
2021-04-07 13:00:44 +0530

    commenting in shared files added

--- src/pages/Dashboard/sharedfiles.jsx
+++ src/pages/Dashboard/sharedfiles.jsx
@@ -17,18 +17,19 @@ import FileSaver from "file-saver";
 import mime from "mime-types";
 
 const SharedFiles = ({ privateKey, setPrivateKey }) => {
+  // Setting up empty state for the state variables
   const [SharedFiles, setSharedFiles] = React.useState([]); // Use this when you set up the IPFS thing.
   const [contract, setContract] = React.useState("");
   const [username, setUsername] = React.useState("");
   const [checked_index, setindex] = React.useState([]);
   const [checked, setChecked] = React.useState([]);
-
   const [modalVisible, setModalVisible] = React.useState(false);
   const [keyFile, setKeyFile] = React.useState();
   const [nameDialog, setUnameDialog] = React.useState(false);
   const [checkedstyle, setCheckedStyle] = React.useState({});
   const [message, setMessage] = React.useState("");
   const [loader, setLoader] = React.useState(false);
+    // function to retrieve the username from the local storage
   function getUserName() {
     const tokenString = localStorage.getItem("user_name");
     if (tokenString) {
@@ -38,7 +39,7 @@ const SharedFiles = ({ privateKey, setPrivateKey }) => {
       return false;
     }
   }
-
+  // preloaded script to load the contract and initialise the web3
   React.useEffect(() => {
     async function setup() {
       await loadWeb3();
@@ -47,8 +48,9 @@ const SharedFiles = ({ privateKey, setPrivateKey }) => {
     }
     setup();
   }, []);
-
+  // function used to handle the change in the check box when there is any change this fucntion will be executed
   const handleChange = (index) => {
+      // creating a dummy array equal to checked and changed the particular index to not of the current index value to show the change
     const check_dummy = checked;
     check_dummy[index] = !check_dummy[index];
     let selectionStyle = {
@@ -56,13 +58,17 @@ const SharedFiles = ({ privateKey, setPrivateKey }) => {
       style: { background: "orange", opacity: "0.7" },
     };
     let unselectionStyle = { index: index, style: {} };
+    // Set the state of checked with the dummy index
     setChecked(check_dummy);
+    // if the checked index value is true then we will push the particular index in the dummy and set the final state of index array
     if (checked[index]) {
       const checked_index_dummmy = checked_index;
       checked_index_dummmy.push(index);
       setindex(checked_index_dummmy);
       setCheckedStyle(selectionStyle);
     }
+    // if the checked index value is false then that means the checkbox is unchecked then we need to remove the particular ndex from the array
+    // once the array is final it will be in the set state 
     if (!checked[index]) {
       const checked_index_dummmy = checked_index;
       const i = checked_index_dummmy.indexOf(index);
@@ -74,67 +80,84 @@ const SharedFiles = ({ privateKey, setPrivateKey }) => {
     }
   };
 
-  async function setup() {
-    await loadWeb3();
-    const Contract = await ContractConnect();
-    setContract(Contract);
-  }
-
+  // function used for geting the my files list from web3
   async function setupSharedFiles() {
+      // getting the username from the local storage
     const user = getUserName();
+      // if the username and contract are set with the value then this block will be executed
     setUsername(user);
 
     if (contract && username) {
+      // getting the file hashes json array from the smart contract passing the contract and username
       const filehashes = await GetShareFiles(contract, username);
 
       if (filehashes) {
         setSharedFiles(filehashes);
+        // setting the false value to the array with the length of the filehashes 
         setChecked(Array(SharedFiles.length).fill(false));
       }
     }
   }
-
+  // This function is used to read the file for setting the private key value in the state from the select file
   function readkeyFile(file) {
     var reader = new FileReader();
     reader.readAsText(file, "UTF-8");
     reader.onload = (evt) => {
+        // setting the private key state with the private key text in the file
       setPrivateKey(evt.target.result);
     };
+    // If it found error it prints the error in the console.log
     reader.onerror = () => console.log("error");
   }
-
+  // Function for handling the download files 
   async function handleDownloadFiles() {
+      // if the private eky is initialised and any atleast one item in checked then this block will be executed
     if (privateKey && checked_index.length >= 0) {
+      // we will start the loader
       setLoader(true);
+      // setting the status of the action 
       setMessage(
         "We are processing your download files. Please wait for a while. If browser prompts, click on wait."
       );
+      // looping the checked index and performing the whole download logic here
       checked_index.map(async (value, j) => {
+        // getting the file name from sharedfiles retrived from the smart contract
         const file_n = SharedFiles[checked_index[j]].filename;
+        // getting the filehash from the sharedfiles retrived from the smart contract
         const file_h = SharedFiles[checked_index[j]].filehash;
+        // get the info of the sender
         const sender = SharedFiles[checked_index[j]].sender;
+        // get the public hash of the sender for public key
         const sender_public_hash = await GetPublic(contract, sender);
+        // get the public key of the sender for public key from ipfs
         const sender_public_key = await StringRetrive(sender_public_hash);
+        // get the encrypted file from the filehash
         const encrypted_file = await FileRetrive(file_h);
+        // decrypt with the private key and return the file buffer
         const decrypt_receiver = await DefaultDecryptPrivateKey(
           encrypted_file,
           privateKey
         );
+        // decrypt again with the sender public key as file buffer returns
         const decrypt_sender = await DefaultDecryptPublicKeyFile(
           decrypt_receiver,
           sender_public_key
         );
+        // Getting the content type of the file
         const type = mime.lookup(file_n);
+        // Converting the file buffer to blob
         var blob = new Blob([decrypt_sender], {
           type: type,
         });
+        // using the file saver library to download the blob
         FileSaver.saveAs(blob, file_n);
         setLoader(false);
+        // setting the status message for successful downlaod
         setMessage("Files Decrypted and saved. Please check downloads.");
       });
     }
   };
-
+  // Preload script to get the shared files from smart contract
   React.useEffect(() => {
     setupSharedFiles();
   }, [contract]);
