beba282912e55c64fd0920ef494940fed4e0f117
Sameer Kashyap <sameera.s.kashyap@gmail.com>
2021-04-07 16:04:07 +0530

    reduce fontsize

--- src/components/FileBar/index.jsx
+++ src/components/FileBar/index.jsx
@@ -4,16 +4,31 @@
  * Every filebar component has Filename and Sizes, computed in MBs
  */
 
+import React from "react";
 
-import React from 'react'
-
-const FileBar =(props)=>{
-return(
-    <div style={{height:"55px", width:'100%', display:'flex', flexDirection:'row', justifyContent:'space-around'}}>
-        <span style={{flexBasis:3}}><b>Filename</b><br/>{props.filename}</span><br/>
-        <span  style={{flexBasis:1}}><b>Size(MB)</b>:<br/>{props.filesize/1000000}</span>
+const FileBar = (props) => {
+  return (
+    <div
+      style={{
+        height: "55px",
+        width: "100%",
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-around",
+      }}
+    >
+      <span>
+        <b>Filename</b>
+        <br />
+        {props.filename}
+        <br />
+      </span>
+      <span>
+        <b>Size(MB)</b>:<br />
+        {props.filesize / 1000000}
+      </span>
     </div>
-)
-}
+  );
+};
 
 export default FileBar;
