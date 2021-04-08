72519a9822b8fc60a81e17334dce916afd6e3956
imabp <abir.pal899@gmail.com>
2021-04-09 01:19:14 +0530

    Updated Discord Link

--- src/pages/Landing/src/components/sections/Cta.js
+++ src/pages/Landing/src/components/sections/Cta.js
@@ -46,25 +46,9 @@ const Cta = ({
       <div className="container">
         <div className={innerClasses}>
           <div className="cta-slogan">
-            <h3 className="m-0">Have questions? Get in touch</h3>
+            <h3 className="m-0">Have questions? <br/>Get in touch on our <a href="https://discord.gg/BKgbJFS4Kf"><u>Discord</u></a></h3>
           </div>
-          <div className="cta-action">
-            <Input
-              id="newsletter"
-              type="email"
-              label="Subscribe"
-              labelHidden
-              hasIcon="right"
-              placeholder="Your email"
-            >
-              <svg width="16" height="12" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M9 5H1c-.6 0-1 .4-1 1s.4 1 1 1h8v5l7-6-7-6v5z"
-                  fill="#376DF9"
-                />
-              </svg>
-            </Input>
-          </div>
+        
         </div>
       </div>
     </section>
